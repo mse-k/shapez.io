e6d07f5cc766b6e643aec32dd363eec2a4ddef8f
mse <42069+mse@users.noreply.github.com>
2020-06-05 17:43:50 -0400

    what?

--- src/js/game/item_registry.js
+++
@@ -1,8 +0,0 @@
-import { gItemRegistry } from "../core/global_registries";
-import { ShapeItem } from "./items/shape_item";
-import { ColorItem } from "./items/color_item";
-
-export function initItemRegistry() {
-    gItemRegistry.register(ShapeItem);
-    gItemRegistry.register(ColorItem);
-}
